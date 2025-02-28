4f720b22fe63a577b5b9e9a8710d935ffea0b0ef
Sander van Hooft <info@sandervanhooft.nl>
2025-02-28 17:27:11 +0100

    wip

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,14 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'moneybird php',
+			logo: {
+				src: './src/assets/moneybird_logo.svg',
+				alt: 'Moneybird logo',
+				replacesTitle: true,
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
 			social: {
 				github: 'https://github.com/sandorian/moneybird-api-php',
 			},
