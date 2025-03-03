a43bfbc35ab498aadd783cf012c1bed85d17181f
Sander van Hooft <info@sandervanhooft.nl>
2025-03-03 13:46:49 +0100

    gh docs deploy action

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sandorian.github.io/moneybird-api-php',
 	integrations: [
 		starlight({
 			title: 'moneybird php',
